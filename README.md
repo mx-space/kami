920da7031d2e10f581a86712b46ab0397738c9de
Innei <tukon479@gmail.com>
2020-04-13 20:23:19 +0800

    init: first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# nextjs with typeScript + express + nodemon example
+
+froked from [next.js/examples/custom-server-typecript](https://github.com/zeit/next.js/tree/canary/examples/custom-server-typescript) and build express
+
+Install it and run:
+
+```bash
+npm install
+npm run dev
+# or
+yarn
+yarn dev
+```
---
+++ server/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import next from 'next'
+
+const port = parseInt(process.env.PORT || '2323', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  const server = express()
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  server.listen(port, (err: any) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
