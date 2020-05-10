e8533f164eaa5295146b417eaeb91285434ea709
Innei <tukon479@gmail.com>
2020-05-10 11:16:21 +0800

    feat: log

--- build/server/index.js
+++ build/server/index.js
@@ -9,15 +9,18 @@ const routers_1 = require("./routers");
 const cors_1 = __importDefault(require("cors"));
 const apicache_1 = __importDefault(require("apicache"));
 const redis_1 = __importDefault(require("redis"));
+const morgan_1 = __importDefault(require("morgan"));
 const port = parseInt(process.env.PORT || '2323', 10);
 const dev = process.env.NODE_ENV !== 'production';
 const app = next_1.default({ dev });
 const handle = app.getRequestHandler();
 // @ts-ignore
 const REDIS = Boolean(~~process.env.REDIS || 0);
-console.log(REDIS, typeof REDIS);
-app.prepare().then(() => {
+app
+    .prepare()
+    .then(() => {
     const server = express_1.default();
+    server.use(dev ? morgan_1.default('dev') : morgan_1.default('short'));
     const cache = process.env.NODE_ENV === 'production'
         ? REDIS
             ? apicache_1.default.options({ redisClient: redis_1.default.createClient() }).middleware
@@ -34,9 +37,25 @@ app.prepare().then(() => {
     server.get('*', cache('10 minutes'), (req, res) => {
         return handle(req, res);
     });
+    server.use((err, req, res, next) => {
+        if (dev) {
+            console.error(err);
+        }
+        else {
+            console.log(err.message);
+        }
+    });
     server.listen(port, (err) => {
         if (err)
             throw err;
         console.log(`> Ready on http://localhost:${port}`);
     });
+})
+    .catch((e) => {
+    if (dev) {
+        console.error(e);
+    }
+    else {
+        console.log(e.message);
+    }
 });
--- server/index.ts
+++ server/index.ts
@@ -4,35 +4,54 @@ import { router } from './routers'
 import cors from 'cors'
 import apicache from 'apicache'
 import redis from 'redis'
+import morgan from 'morgan'
+
 const port = parseInt(process.env.PORT || '2323', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+
 // @ts-ignore
 const REDIS = Boolean(~~process.env.REDIS || 0)
-console.log(REDIS, typeof REDIS)
-app.prepare().then(() => {
-  const server = express()
-  const cache =
-    process.env.NODE_ENV === 'production'
-      ? REDIS
-        ? apicache.options({ redisClient: redis.createClient() }).middleware
-        : apicache.middleware
-      : () => (req, res, next) => next()
-  server.use(cors())
-  server.use('/_extra', router)
-  server.get('/feed', cache('120 minutes'), (req, res) => {
-    return handle(req, res)
-  })
-  server.get('/atom.xml', cache('120 minutes'), (req, res) => {
-    return handle(req, res)
-  })
-  server.get('*', cache('10 minutes'), (req, res) => {
-    return handle(req, res)
-  })
 
-  server.listen(port, (err: any) => {
-    if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+app
+  .prepare()
+  .then(() => {
+    const server = express()
+    server.use(dev ? morgan('dev') : morgan('short'))
+    const cache =
+      process.env.NODE_ENV === 'production'
+        ? REDIS
+          ? apicache.options({ redisClient: redis.createClient() }).middleware
+          : apicache.middleware
+        : () => (req, res, next) => next()
+    server.use(cors())
+    server.use('/_extra', router)
+    server.get('/feed', cache('120 minutes'), (req, res) => {
+      return handle(req, res)
+    })
+    server.get('/atom.xml', cache('120 minutes'), (req, res) => {
+      return handle(req, res)
+    })
+    server.get('*', cache('10 minutes'), (req, res) => {
+      return handle(req, res)
+    })
+    server.use((err, req, res, next) => {
+      if (dev) {
+        console.error(err)
+      } else {
+        console.log(err.message)
+      }
+    })
+    server.listen(port, (err: any) => {
+      if (err) throw err
+      console.log(`> Ready on http://localhost:${port}`)
+    })
   })
-})
+  .catch((e) => {
+    if (dev) {
+      console.error(e)
+    } else {
+      console.log(e.message)
+    }
+  })
