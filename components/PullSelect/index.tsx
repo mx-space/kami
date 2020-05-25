58b3eee3e03619660cef415eda6ec36457c560a1
Innei <tukon479@gmail.com>
2020-05-25 13:41:47 +0800

    feat: switch animate

--- components/PullSelect/index.tsx
+++ components/PullSelect/index.tsx
@@ -1,16 +1,30 @@
-import { FC, DetailedHTMLProps, HTMLAttributes } from 'react'
+import { FC, DetailedHTMLProps, HTMLAttributes, useRef } from 'react'
 import styles from './index.module.scss'
 export const PullSelect: FC<DetailedHTMLProps<
   HTMLAttributes<HTMLDivElement>,
   HTMLDivElement
 >> = (props = {}) => {
+  const containerRef = useRef<HTMLDivElement>(null)
   return (
-    <div className={styles['select-container']}>
+    <div className={styles['select-container']} ref={containerRef}>
       <div className={styles['select-line']}>
         <div className={styles['line']}></div>
       </div>
       <div className={styles['sakura-wrap']} {...props}>
-        <div className={styles['sakura-img']}></div>
+        <div
+          className={styles['sakura-img']}
+          onClick={(_) => {
+            if (containerRef.current) {
+              containerRef.current.style.top = '0'
+              setTimeout(() => {
+                try {
+                  containerRef.current!.style.top = ''
+                  // eslint-disable-next-line no-empty
+                } catch {}
+              }, 1000)
+            }
+          }}
+        ></div>
       </div>
     </div>
   )
