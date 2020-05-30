1fc517659d46b80b1092b94d69b8a0d937e1a871
Innei <tukon479@gmail.com>
2020-05-30 11:24:25 +0800

    fix: white mode color

--- components/SectionNews/section.tsx
+++ components/SectionNews/section.tsx
@@ -7,6 +7,8 @@ import Link from 'next/link'
 import randomColor from 'randomcolor'
 import { DetailedHTMLProps, FC, forwardRef, HTMLAttributes } from 'react'
 import styles from './index.module.scss'
+import { observer } from 'mobx-react'
+import { useStore } from '../../store'
 export interface SectionNewsProps {
   title: string
   icon: IconDefinition
@@ -20,58 +22,65 @@ export interface SectionNewsProps {
 export const SectionWrap: FC<
   SectionNewsProps &
     DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>
-> = forwardRef((props, ref) => {
-  const {
-    title,
-    icon,
-    moreUrl,
-    color = randomColor({
-      luminosity: 'dark',
-    }),
-    showMoreIcon = true,
-    ...rest
-  } = props
-  return (
-    <>
-      <style jsx>
-        {`
-          h3 {
-            transition: all 0.5s;
-          }
-        `}
-      </style>
-      <div className="news-item" ref={ref as any}>
-        <div className="news-head">
-          <h3
-            className="title"
-            style={{
-              backgroundColor: color,
-            }}
-          >
-            <FontAwesomeIcon icon={icon} className={styles.icon} />
-            {title}
-          </h3>
-          {showMoreIcon && moreUrl && (
+> = observer(
+  forwardRef((props, ref) => {
+    const {
+      title,
+      icon,
+      moreUrl,
+      color = randomColor({
+        luminosity: 'dark',
+      }),
+      showMoreIcon = true,
+      ...rest
+    } = props
+    const { appStore } = useStore()
+    const mode = appStore.colorMode
+    return (
+      <>
+        <style jsx>
+          {`
+            h3 {
+              transition: all 0.5s;
+            }
+          `}
+        </style>
+        <div className="news-item" ref={ref as any}>
+          <div className="news-head">
             <h3
-              className="more"
+              className="title"
               style={{
                 backgroundColor: color,
+                filter: mode === 'dark' ? 'brightness(0.8)' : 'brightness(1.5)',
               }}
             >
-              <Link href={moreUrl}>
-                <a>
-                  <FontAwesomeIcon icon={faChevronRight} />
-                </a>
-              </Link>
+              <FontAwesomeIcon icon={icon} className={styles.icon} />
+              {title}
             </h3>
-          )}
-        </div>
-        <div className="news-body">
-          <div className="row s" {...rest}>
-            {props.children}
+            {showMoreIcon && moreUrl && (
+              <h3
+                className="more"
+                style={{
+                  backgroundColor: color,
+                  filter:
+                    mode === 'dark' ? 'brightness(0.8)' : 'brightness(1.5)',
+                }}
+              >
+                <Link href={moreUrl}>
+                  <a>
+                    <FontAwesomeIcon icon={faChevronRight} />
+                  </a>
+                </Link>
+              </h3>
+            )}
+          </div>
+          <div className="news-body">
+            <div className="row s" {...rest}>
+              {props.children}
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  )
-})
+      </>
+    )
+  }),
+)
