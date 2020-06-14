98f85636cba250a47eab3d0cd5a471cd5c69a20a
Innei <tukon479@gmail.com>
2020-06-14 22:10:48 +0800

    fix: prism highlight light style

--- components/CodeBlock/index.tsx
+++ components/CodeBlock/index.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react'
 import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { useStore } from '../../common/store'
 import dark from 'react-syntax-highlighter/dist/cjs/styles/prism/tomorrow'
+import light from 'react-syntax-highlighter/dist/cjs/styles/prism/prism'
 import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript'
 import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css'
 import java from 'react-syntax-highlighter/dist/cjs/languages/prism/java'
@@ -87,7 +88,7 @@ const CodeBlock: FC<CodeBlockProps> = observer((props) => {
   return (
     <SyntaxHighlighter
       language={language}
-      style={colorMode === 'dark' ? dark : undefined}
+      style={colorMode === 'dark' ? dark : light}
       customStyle={{
         background: 'var(--code-bg) !important',
       }}
