f2f50f665fd8c745488e8571b22e2b09734fc846
Innei <tukon479@gmail.com>
2020-12-04 14:32:36 +0800

    chore(deps): update deps and fix hacky react generic react hoc

--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames'
 import {
   DetailedHTMLProps,
+  ForwardedRef,
   InputHTMLAttributes,
   useEffect,
   useMemo,
@@ -26,61 +27,64 @@ type IInputProps = Omit<
     >,
     'prefix'
   >
-export const Input = ensuredForwardRef<HTMLInputElement, IInputProps>(
-  (props, ref) => {
-    // const [value, setValue] = useState(props.value)
-    const { prefix, value, onChange, multi = false, ...rest } = props
-    const [focused, setFocus] = useState(false)
-    const inputWrapRef = useRef<HTMLSpanElement>(null)
-    const [size, setSize] = useState({
-      height: 0,
-      width: 0,
-    })
-    const C = useMemo(() => (size.height + size.width) * 2, [size])
-    useEffect(() => {
-      requestAnimationFrame(() => {
-        if (!inputWrapRef.current) {
-          return
-        }
-        const $input = inputWrapRef.current
+export const Input = ensuredForwardRef<
+  HTMLInputElement | HTMLTextAreaElement,
+  IInputProps
+>((props, ref) => {
+  // const [value, setValue] = useState(props.value)
+  const { prefix, value, onChange, multi = false, ...rest } = props
+  const [focused, setFocus] = useState(false)
+  const inputWrapRef = useRef<HTMLSpanElement>(null)
+  const [size, setSize] = useState({
+    height: 0,
+    width: 0,
+  })
+  const C = useMemo(() => (size.height + size.width) * 2, [size])
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      if (!inputWrapRef.current) {
+        return
+      }
+      const $input = inputWrapRef.current
 
-        const size = {
-          height: $input.clientHeight,
-          width: $input.clientWidth,
-        }
-        setSize(size)
-      })
-    }, [inputWrapRef])
-    const I = (multi ? 'textarea' : 'input') as keyof JSX.IntrinsicElements
-    return (
-      <span
-        className={classNames(
-          styles['input-wrap'],
-          focused ? styles['focus'] : undefined,
-        )}
-        ref={inputWrapRef}
-      >
-        {prefix && <div className={styles['prefix-wrap']}>{prefix}</div>}
-        {typeof C === 'number' && !!C && (
-          <div className={styles['border']}>
-            <svg>
-              <rect
-                height={size.height}
-                width={size.width}
-                // x={1}
-                // y={0}
-                style={{
-                  strokeDasharray: `${C}px`,
-                  strokeDashoffset: !focused ? `${C}px` : undefined,
-                  // '--C': `${C}px`,
-                }}
-                className={styles['rect']}
-              ></rect>
-            </svg>
-          </div>
-        )}
-        <I
-          ref={ref}
+      const size = {
+        height: $input.clientHeight,
+        width: $input.clientWidth,
+      }
+      setSize(size)
+    })
+  }, [inputWrapRef])
+
+  return (
+    <span
+      className={classNames(
+        styles['input-wrap'],
+        focused ? styles['focus'] : undefined,
+      )}
+      ref={inputWrapRef}
+    >
+      {prefix && <div className={styles['prefix-wrap']}>{prefix}</div>}
+      {typeof C === 'number' && !!C && (
+        <div className={styles['border']}>
+          <svg>
+            <rect
+              height={size.height}
+              width={size.width}
+              // x={1}
+              // y={0}
+              style={{
+                strokeDasharray: `${C}px`,
+                strokeDashoffset: !focused ? `${C}px` : undefined,
+                // '--C': `${C}px`,
+              }}
+              className={styles['rect']}
+            ></rect>
+          </svg>
+        </div>
+      )}
+      {multi ? (
+        <textarea
+          ref={ref as ForwardedRef<HTMLTextAreaElement>}
           value={value}
           {...rest}
           onFocus={(e) => {
@@ -98,7 +102,27 @@ export const Input = ensuredForwardRef<HTMLInputElement, IInputProps>(
             prefix ? styles['has-prefix'] : null,
           )}
         />
-      </span>
-    )
-  },
-)
+      ) : (
+        <input
+          ref={ref as ForwardedRef<HTMLInputElement>}
+          value={value}
+          {...rest}
+          onFocus={(e) => {
+            setFocus(true)
+            props.onFocus?.(e)
+          }}
+          onBlur={(e) => {
+            setFocus(false)
+            props.onBlur?.(e)
+          }}
+          // @ts-ignore
+          onChange={onChange}
+          className={classNames(
+            styles['input'],
+            prefix ? styles['has-prefix'] : null,
+          )}
+        />
+      )}
+    </span>
+  )
+})
