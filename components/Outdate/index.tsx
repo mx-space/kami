0fe1b5ff949bdf0081788f2613b506099a76293d
Innei <tukon479@gmail.com>
2021-02-16 21:14:34 +0800

    fix: outdate block style

--- components/Outdate/index.tsx
+++ components/Outdate/index.tsx
@@ -1,7 +1,7 @@
 /*
  * @Author: Innei
  * @Date: 2020-09-17 14:02:24
- * @LastEditTime: 2021-01-26 11:33:00
+ * @LastEditTime: 2021-02-16 21:13:38
  * @LastEditors: Innei
  * @FilePath: /web/components/Outdate/index.tsx
  * @Mark: Coding with Love
@@ -10,9 +10,10 @@ import { RelativeTime } from 'components/Time'
 import dayjs from 'dayjs'
 import React from 'react'
 
+// TODO re-design style
 export default (function OutdateNotice({ time }: { time: string | Date }) {
   return dayjs().diff(dayjs(time), 'day') > 60 ? (
-    <blockquote>
+    <blockquote style={{ marginBottom: '5rem' }}>
       这篇文章上次修改于 <RelativeTime date={new Date(time)} />
       ，可能其部分内容已经发生变化，如有疑问可询问作者。
     </blockquote>
