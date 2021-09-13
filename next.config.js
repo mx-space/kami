4e2d5f502f37a572245440914d636b459e65c1c9
Innei <tukon479@gmail.com>
2021-09-13 20:49:45 +0800

    fix: next config

--- next.config.js
+++ next.config.js
@@ -41,16 +41,19 @@ const configs = withBundleAnalyzer({
         source: '/service-worker.js',
         destination: '/_next/static/service-worker.js',
       },
-      {
-        source: '/api/:path*',
-        destination: env.NEXT_PUBLIC_APIURL + '/:path*',
-      },
     ]
     // this can remove after test
     if (isProd && env.ASSETPREFIX) {
       base.push({
         source: '/autostatic/:path*',
         destination: env.ASSETPREFIX + '/_next/static/:path*',
+      })
+    }
+
+    if (env.NEXT_PUBLIC_APIURL) {
+      base.push({
+        source: '/api/:path*',
+        destination: env.NEXT_PUBLIC_APIURL + '/:path*',
       })
     }
     return base
