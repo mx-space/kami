9d21eabf83e53b5a0ced3379a65b27b2c5dbbecb
Innei <tukon479@gmail.com>
2021-10-31 14:21:57 +0800

    feat: pwa replacement

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 const env = require('dotenv').config().parsed || {}
+const withPWA = require('next-pwa')
 
-const withOffline = require('next-offline')
 const configs = withBundleAnalyzer({
   webpack: (config, options) => {
     // config.experiments = {
@@ -27,18 +27,7 @@ const configs = withBundleAnalyzer({
       { source: '/feed', destination: '/api/feed' },
       { source: '/rss', destination: '/api/feed' },
       { source: '/atom.xml', destination: '/api/feed' },
-      {
-        source: '/service-worker.js',
-        destination: '/_next/static/service-worker.js',
-      },
     ]
-    // this can remove after test
-    if (isProd && env.ASSETPREFIX) {
-      base.push({
-        source: '/autostatic/:path*',
-        destination: env.ASSETPREFIX + '/_next/static/:path*',
-      })
-    }
 
     if (env.NEXT_PUBLIC_APIURL) {
       base.push({
@@ -51,41 +40,17 @@ const configs = withBundleAnalyzer({
   // 小水管就算了吧
   // __NEXT_OPTIMIZE_FONTS=true 暂时
   optimizeFonts: false,
-  // experimental: {
-  //   granularChunks: true,
-  //   modern: true,
-  //   scrollRestoration: true,
-  // },
 })
 
-module.exports = isProd
-  ? withOffline({
-      workboxOpts: {
-        swDest: process.env.NEXT_EXPORT
-          ? 'service-worker.js'
-          : 'static/service-worker.js',
-        modifyURLPrefix: {
-          // @see: https://github.com/hanford/next-offline/issues/263#issuecomment-738155607
-          'autostatic/':
-            (env.ASSETPREFIX ? env.ASSETPREFIX + '/' : '') + '_next/static/', // new addition
-          'static/':
-            (env.ASSETPREFIX ? env.ASSETPREFIX + '/' : '') + '_next/static/',
-          'public/':
-            (env.ASSETPREFIX ? env.ASSETPREFIX + '/' : '') + '_next/public/',
-        },
-        runtimeCaching: [
-          {
-            urlPattern: /^https?.*/,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'offlineCache',
-              expiration: {
-                maxEntries: 200,
-              },
-            },
-          },
-        ],
-      },
-      ...configs,
-    })
-  : configs
+module.exports = withPWA({
+  ...configs,
+  pwa: {
+    // FIXME: generate wrong asset path
+    // @see: https://github.com/shadowwalker/next-pwa/issues/289
+    dest: 'public',
+
+    register: true,
+    skipWaiting: true,
+    disable: !isProd,
+  },
+})
