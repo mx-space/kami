914dacbe1ceb194d33ebaa74da5b184e3416e263
Innei <tukon479@gmail.com>
2022-01-24 14:41:03 +0800

    fix: dark mode detect if system mode

--- src/hooks/use-media-toggle.ts
+++ src/hooks/use-media-toggle.ts
@@ -10,17 +10,31 @@ const mockElement = {
     remove: noop,
   },
 }
-
+const darkModeKey = 'darkMode'
 export const useMediaToggle = () => {
   const { appStore: app } = useStore()
   const { disable, enable, toggle, value } = useDarkMode(false, {
     classNameDark: 'dark',
     classNameLight: 'light',
+    storageKey: darkModeKey,
     element: (globalThis.document && document.documentElement) || mockElement,
   })
 
   useEffect(() => {
     app.colorMode = value ? 'dark' : 'light'
+  }, [value])
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.matchMedia('(prefers-color-scheme: dark)').matches && value) {
+        localStorage.removeItem(darkModeKey)
+      }
+    }
+    window.addEventListener('beforeunload', handler)
+
+    return () => {
+      window.removeEventListener('beforeunload', handler)
+    }
   }, [value])
 
   return {
