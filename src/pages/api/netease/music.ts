6ae51d0dbefd851cf2fa38e3e0b8e7d63573173b
Innei <tukon479@gmail.com>
2022-02-11 21:31:33 +0800

    fix(music): remove active transtion music cover

--- src/pages/api/netease/music.ts
+++ src/pages/api/netease/music.ts
@@ -1,16 +1,6 @@
-/*
- * @Author: Innei
- * @Date: 2021-02-11 13:54:34
- * @LastEditTime: 2021-06-28 11:26:48
- * @LastEditors: Innei
- * @FilePath: /web/pages/api/netease/music.ts
- * @Mark: Coding with Love
- */
-
 import { NeteaseMusic } from '@mx-space/extra'
 import { IncomingMessage, ServerResponse } from 'http'
 import { writeBody } from 'utils'
-
 export default async function (req: IncomingMessage, res: ServerResponse) {
   try {
     const { NETEASE_PHONE, NETEASE_PASSWORD } = process.env
