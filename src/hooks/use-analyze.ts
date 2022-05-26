2743d599272ebb4284676f8a5f8dcf00f4fe1547
Innei <tukon479@gmail.com>
2022-05-26 12:36:51 +0800

    feat: add baidu tracker

--- src/hooks/use-analyze.ts
+++ src/hooks/use-analyze.ts
@@ -5,6 +5,19 @@ import { isDev } from 'utils'
 
 import { useThemeConfig } from './use-initial-data'
 
+declare global {
+  interface Window {
+    // 百度统计
+    _hmt?: {
+      /**
+       * https://tongji.baidu.com/holmes/Tongji/%E7%BB%9F%E8%AE%A1%E5%BC%80%E6%94%BE%E6%89%8B%E5%86%8C/JS-API%E6%8A%80%E6%9C%AF%E6%96%87%E6%A1%A3/_trackEvent
+       * @param options
+       */
+      push(options: any): void
+    }
+  }
+}
+
 export const useAnalyze = () => {
   const config = useThemeConfig()
   const { userStore } = useStore()
@@ -22,6 +35,7 @@ export const useAnalyze = () => {
       })
 
       window.umami?.trackView(url)
+      window._hmt?.push(['_trackPageview', url])
     },
     [GA_TRACKING_ID, isEnableGA],
   )
@@ -49,6 +63,8 @@ export const useAnalyze = () => {
           value,
         })
         window.umami?.trackEvent(label || value?.toString() || '', action)
+
+        window._hmt?.push(['_trackEvent', category || 'event', action, label])
       } catch (err) {
         console.log(err)
       }
