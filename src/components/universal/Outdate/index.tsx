fed85744822e39d9272c7a759ac7452ccf38ba77
Innei <tukon479@gmail.com>
2022-06-18 21:18:28 +0800

    fix: ui improve

--- src/components/universal/Outdate/index.tsx
+++ src/components/universal/Outdate/index.tsx
@@ -1,20 +1,11 @@
-/*
- * @Author: Innei
- * @Date: 2020-09-17 14:02:24
- * @LastEditTime: 2021-02-16 21:13:38
- * @LastEditors: Innei
- * @FilePath: /web/components/Outdate/index.tsx
- * @Mark: Coding with Love
- */
 import dayjs from 'dayjs'
 import React from 'react'
 
 import { RelativeTime } from '~/components/universal/RelativeTime'
 
-// TODO re-design style
 export default (function OutdateNotice({ time }: { time: string | Date }) {
   return dayjs().diff(dayjs(time), 'day') > 60 ? (
-    <blockquote style={{ marginBottom: '5rem' }}>
+    <blockquote className="mb-20">
       这篇文章上次修改于 <RelativeTime date={new Date(time)} />
       ，可能其部分内容已经发生变化，如有疑问可询问作者。
     </blockquote>
