fc10dc696ec644d454fcb7f30994a2d1aa5099ed
Innei <tukon479@gmail.com>
2022-06-30 22:12:56 +0800

    chore: add todo

--- src/components/universal/Error/no-data.tsx
+++ src/components/universal/Error/no-data.tsx
@@ -6,17 +6,26 @@ import { ErrorView } from '.'
 
 export const NoDataErrorView: FC = () => {
   return (
-    <ErrorView
-      noSeo
-      statusCode={'无数据'}
-      showBackButton={false}
-      description={
-        <>
-          <p>出现这个错误表示未获取到初始数据</p>
-          <p>可能是 API 接口地址配置不正确, 或者后端服务出现异常</p>
-          <p>API 地址: {API_URL}</p>
-        </>
-      }
-    />
+    <>
+      <ErrorView
+        noSeo
+        statusCode={'无数据'}
+        showBackButton={false}
+        description={
+          <>
+            <p>出现这个错误表示未获取到初始数据</p>
+            <p>可能是 API 接口地址配置不正确, 或者后端服务出现异常</p>
+            <p>API 地址: {API_URL}</p>
+          </>
+        }
+      />
+
+      <div
+        className="absolute opacity-20 blur-15 filter inset-0 bg-center bg-cover"
+        style={{
+          backgroundImage: `url("https://fastly.jsdelivr.net/gh/mx-space/docs-images@master/images/chichi-1.jpeg")`,
+        }}
+      ></div>
+    </>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -138,6 +138,7 @@ App.getInitialProps = async (props: AppContext) => {
       // Next 会从小组件向上渲染整个页面，有可能在此报错。兜底
       return await NextApp.getInitialProps(props)
     } catch (e) {
+      // TODO next rfc Layout, 出了就重构这里
       // 只有无数据 也就是 服务端不跑起来 或者接口不对的时候 捕获异常
       // 这是为什么呢 说来说去还是 nextjs 太辣鸡了 只能各种 hack
       // 只能这样了
