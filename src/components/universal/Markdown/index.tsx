c49fefde2c419480cca518385a7de293b4e02a94
Innei <tukon479@gmail.com>
2022-12-05 23:49:57 +0800

    feat: replace new markdown component
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/components/universal/Markdown/index.tsx
+++ src/components/universal/Markdown/index.tsx
@@ -1,20 +1,13 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import { clsx } from 'clsx'
-import range from 'lodash-es/range'
 import type { MarkdownToJSX } from 'markdown-to-jsx'
-import { compiler, sanitizeUrl } from 'markdown-to-jsx'
+import { sanitizeUrl } from 'markdown-to-jsx'
 import { observer } from 'mobx-react-lite'
 import dynamic from 'next/dynamic'
 import type { FC } from 'react'
-import React, {
-  Fragment,
-  memo,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react'
+import React, { Fragment, memo, useCallback, useEffect, useMemo } from 'react'
+
+import type { MdProps } from '@mx-space/kami-design/components/Markdown'
+import { Markdown as KamiMarkdown } from '@mx-space/kami-design/components/Markdown'
 
 import { ErrorBoundary } from '~/components/app/ErrorBoundary'
 import type { TocProps } from '~/components/widgets/Toc'
@@ -26,24 +19,7 @@ import { CodeBlock } from '../CodeBlock'
 import { FloatPopover } from '../FloatPopover'
 import { FluentList16Filled } from '../Icons/shared'
 import { useModalStack } from '../Modal/stack.context'
-import styles from './index.module.css'
-import { CommentAtRule } from './parsers/comment-at'
-import { ContainerRule } from './parsers/container'
-import { InsertRule } from './parsers/ins'
-import { KateXRule } from './parsers/katex'
-import { MarkRule } from './parsers/mark'
-import { MentionRule } from './parsers/mention'
-import { SpoilderRule } from './parsers/spoiler'
-import {
-  MHeading,
-  MImage,
-  MLink,
-  MParagraph,
-  MTableBody,
-  MTableHead,
-  MTableRow,
-} from './renderers'
-import { MDetails } from './renderers/collapse'
+import { MHeading, MImage, MLink } from './renderers'
 import { MFootNote } from './renderers/footnotes'
 import { LinkCard } from './renderers/link-card'
 
@@ -53,217 +29,22 @@ const Toc = dynamic(
     ssr: false,
   },
 )
-interface MdProps {
-  value?: string
-  toc?: boolean
-
-  style?: React.CSSProperties
-  readonly renderers?: { [key: string]: Partial<MarkdownToJSX.Rule> }
-  wrapperProps?: React.DetailedHTMLProps<
-    React.HTMLAttributes<HTMLDivElement>,
-    HTMLDivElement
-  >
-  codeBlockFully?: boolean
-  className?: string
-}
 
 export const Markdown: FC<MdProps & MarkdownToJSX.Options> = memo((props) => {
   const {
     value,
     renderers,
-    style,
+
     wrapperProps = {},
-    codeBlockFully = false,
-    className,
-    overrides,
+
     extendsRules,
-    additionalParserRules,
 
     ...rest
   } = props
 
-  const [headings, setHeadings] = useState<HTMLElement[]>([])
-
-  const ref = useRef<HTMLDivElement>(null)
-  useEffect(() => {
-    if (!ref.current) {
-      return
-    }
-
-    const $headings = ref.current.querySelectorAll(
-      range(1, 6)
-        .map((i) => `h${i}`)
-        .join(','),
-    ) as NodeListOf<HTMLHeadingElement>
-
-    setHeadings(Array.from($headings))
-
-    return () => {
-      setHeadings([])
-    }
+  const Heading = useMemo(() => {
+    return MHeading()
   }, [value, props.children])
-
-  const node = useMemo(() => {
-    if (!value && typeof props.children != 'string') return null
-
-    const Heading = MHeading()
-
-    const mdElement = compiler(`${value || props.children}`, {
-      wrapper: null,
-      // @ts-ignore
-      overrides: {
-        p: MParagraph,
-        img: MImage,
-        thead: MTableHead,
-        tr: MTableRow,
-        tbody: MTableBody,
-        // FIXME: footer tag in raw html will renders not as expected, but footer tag in this markdown lib will wrapper as linkReferer footnotes
-        footer: MFootNote,
-        details: MDetails,
-
-        // for custom react component
-        LinkCard,
-        ...overrides,
-      },
-
-      extendsRules: {
-        link: {
-          react(node, output, state) {
-            const { target, title } = node
-            return (
-              <MLink href={sanitizeUrl(target)!} title={title} key={state?.key}>
-                {output(node.content, state!)}
-              </MLink>
-            )
-          },
-        },
-        heading: {
-          react(node, output, state) {
-            return (
-              <Heading id={node.id} level={node.level} key={state?.key}>
-                {output(node.content, state!)}
-              </Heading>
-            )
-          },
-        },
-
-        footnoteReference: {
-          react(node, output, state) {
-            const { footnoteMap, target, content } = node
-            const footnote = footnoteMap.get(content)
-            const linkCardId = (() => {
-              try {
-                const thisUrl = new URL(footnote?.footnote?.replace(': ', ''))
-                const isCurrentHost =
-                  thisUrl.hostname === window.location.hostname
-
-                if (!isCurrentHost && !isDev) {
-                  return undefined
-                }
-                const pathname = thisUrl.pathname
-                return pathname.slice(1)
-              } catch {
-                return undefined
-              }
-            })()
-
-            return (
-              <Fragment key={state?.key}>
-                <a
-                  href={sanitizeUrl(target)!}
-                  onClick={(e) => {
-                    e.preventDefault()
-
-                    springScrollToElement(
-                      document.getElementById(content)!,
-                      undefined,
-                      -window.innerHeight / 2,
-                    )
-                  }}
-                >
-                  <sup key={state?.key}>^{content}</sup>
-                </a>
-                {linkCardId && (
-                  <LinkCard
-                    id={linkCardId}
-                    source={'self'}
-                    // className="float-right"
-                  />
-                )}
-              </Fragment>
-            )
-          },
-        },
-        codeBlock: {
-          react(node, output, state) {
-            return (
-              <CodeBlock
-                key={state?.key}
-                content={node.content}
-                lang={node.lang}
-              />
-            )
-          },
-        },
-        gfmTask: {
-          react(node, _, state) {
-            return (
-              <label className="inline-flex items-center mr-2" key={state?.key}>
-                <input type="checkbox" checked={node.completed} readOnly />
-              </label>
-            )
-          },
-        },
-
-        list: {
-          react(node, output, state) {
-            const Tag = node.ordered ? 'ol' : 'ul'
-
-            return (
-              <Tag key={state?.key} start={node.start}>
-                {node.items.map((item, i) => {
-                  let className = ''
-                  if (item[0]?.type == 'gfmTask') {
-                    className = 'list-none flex items-center'
-                  }
-
-                  return (
-                    <li className={className} key={i}>
-                      {output(item, state!)}
-                    </li>
-                  )
-                })}
-              </Tag>
-            )
-          },
-        },
-
-        ...extendsRules,
-        ...renderers,
-      },
-      additionalParserRules: {
-        spoilder: SpoilderRule,
-        mention: MentionRule,
-        commentAt: CommentAtRule,
-        mark: MarkRule,
-        ins: InsertRule,
-        kateX: KateXRule,
-        container: ContainerRule,
-        ...additionalParserRules,
-      },
-      ...rest,
-    })
-
-    return mdElement
-  }, [
-    value,
-    props.children,
-    overrides,
-    extendsRules,
-    renderers,
-    additionalParserRules,
-    rest,
-  ])
 
   const RenderError = useCallback(
     () => (
@@ -276,21 +57,109 @@ export const Markdown: FC<MdProps & MarkdownToJSX.Options> = memo((props) => {
 
   return (
     <ErrorBoundary fallbackComponent={RenderError}>
-      <div
-        id="write"
-        style={style}
+      <KamiMarkdown
+        tocSlot={
+          props.toc ? ({ headings }) => <TOC headings={headings} /> : () => null
+        }
         {...wrapperProps}
-        ref={ref}
-        className={clsx(
-          styles['md'],
-          codeBlockFully ? styles['code-fully'] : undefined,
-          wrapperProps.className,
-        )}
-      >
-        {className ? <div className={className}>{node}</div> : node}
+        value={value}
+        overrides={{
+          footer: MFootNote,
 
-        {props.toc && <TOC headings={headings} />}
-      </div>
+          img: MImage,
+          // for custom react component
+          LinkCard,
+        }}
+        extendsRules={{
+          link: {
+            react(node, output, state) {
+              const { target, title } = node
+              return (
+                <MLink
+                  href={sanitizeUrl(target)!}
+                  title={title}
+                  key={state?.key}
+                >
+                  {output(node.content, state!)}
+                </MLink>
+              )
+            },
+          },
+          heading: {
+            react(node, output, state) {
+              return (
+                <Heading id={node.id} level={node.level} key={state?.key}>
+                  {output(node.content, state!)}
+                </Heading>
+              )
+            },
+          },
+
+          footnoteReference: {
+            react(node, output, state) {
+              const { footnoteMap, target, content } = node
+              const footnote = footnoteMap.get(content)
+              const linkCardId = (() => {
+                try {
+                  const thisUrl = new URL(footnote?.footnote?.replace(': ', ''))
+                  const isCurrentHost =
+                    thisUrl.hostname === window.location.hostname
+
+                  if (!isCurrentHost && !isDev) {
+                    return undefined
+                  }
+                  const pathname = thisUrl.pathname
+                  return pathname.slice(1)
+                } catch {
+                  return undefined
+                }
+              })()
+
+              return (
+                <Fragment key={state?.key}>
+                  <a
+                    href={sanitizeUrl(target)!}
+                    onClick={(e) => {
+                      e.preventDefault()
+
+                      springScrollToElement(
+                        document.getElementById(content)!,
+                        undefined,
+                        -window.innerHeight / 2,
+                      )
+                    }}
+                  >
+                    <sup key={state?.key}>^{content}</sup>
+                  </a>
+                  {linkCardId && (
+                    <LinkCard
+                      id={linkCardId}
+                      source={'self'}
+                      // className="float-right"
+                    />
+                  )}
+                </Fragment>
+              )
+            },
+          },
+          codeBlock: {
+            react(node, output, state) {
+              return (
+                <CodeBlock
+                  key={state?.key}
+                  content={node.content}
+                  lang={node.lang}
+                />
+              )
+            },
+          },
+          ...extendsRules,
+          ...renderers,
+        }}
+        {...rest}
+      >
+        {props.children}
+      </KamiMarkdown>
     </ErrorBoundary>
   )
 })
