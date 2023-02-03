a9a599f6dd0ce31fb2be90a2458b0a3453b838fa
Innei <tukon479@gmail.com>
2023-02-03 13:13:47 +0800

    fix: google font api, remove loli
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/hooks/use-load-serif-font.ts
+++ src/hooks/use-load-serif-font.ts
@@ -5,7 +5,7 @@ import { loadStyleSheet } from '~/utils/load-script'
 export const useLoadSerifFont = () => {
   useInsertionEffect(() => {
     loadStyleSheet(
-      'https://fonts.loli.net/css?family=Noto+Serif+SC&display=swap',
+      'https://fonts.googleapis.com/css?family=Noto+Serif+SC&display=swap',
     )
   }, [])
 }
