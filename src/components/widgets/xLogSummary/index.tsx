44b5f7aacce15365ed5bdca44c3351502c75579b
Innei <tukon479@gmail.com>
2023-04-05 20:58:23 +0800

    fix: openai icon fill color
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/components/widgets/xLogSummary/index.tsx
+++ src/components/widgets/xLogSummary/index.tsx
@@ -81,6 +81,7 @@ export function OpenAIIcon() {
       role="img"
       height="1em"
       width="1em"
+      fill="currentColor"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
