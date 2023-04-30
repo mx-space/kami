f736dfd2b539b40b0e67048486a57edf5ca683a3
Innei <tukon479@gmail.com>
2023-04-30 20:18:44 +0800

    feat: update table style
    Signed-off-by: Innei <tukon479@gmail.com>

--- packages/kami-design/components/Markdown/renderers/table.tsx
+++ packages/kami-design/components/Markdown/renderers/table.tsx
@@ -1,9 +1,20 @@
+import clsx from 'clsx'
 import type { FC } from 'react'
 import React from 'react'
 
 export const MTableHead: FC<JSX.IntrinsicElements['thead']> = (props) => {
-  const { children, ...rest } = props
-  return <thead {...rest}>{children}</thead>
+  const { children, className, ...rest } = props
+  return (
+    <thead
+      className={clsx(
+        className,
+        'bg-primary text-dark-800 dark:text-light-100 opacity-80',
+      )}
+      {...rest}
+    >
+      {children}
+    </thead>
+  )
 }
 
 export const MTableRow: FC<JSX.IntrinsicElements['tr']> = (props) => {
