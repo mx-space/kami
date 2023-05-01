4696b1c476b8b884d7a27c9eca15707ae1af24ec
Innei <tukon479@gmail.com>
2023-05-01 23:18:22 +0800

    fix: home section margin
    Signed-off-by: Innei <tukon479@gmail.com>

--- src/components/in-page/Home/SectionNews/section.tsx
+++ src/components/in-page/Home/SectionNews/section.tsx
@@ -37,50 +37,45 @@ export const SectionWrap = memo(
     } = props
     const colorMode = useAppStore((state) => state.colorMode)
     return (
-      <>
-        <div className="news-item" ref={ref}>
-          <div className="news-head">
+      <div className="news-item" ref={ref}>
+        <div className="news-head">
+          <h3
+            className="title"
+            style={{
+              backgroundColor: color,
+              filter: colorMode === 'dark' ? 'brightness(0.8)' : undefined,
+            }}
+            suppressHydrationWarning
+          >
+            <div className="absolute left-4 z-1 transform scale-120">
+              {icon}
+            </div>
+
+            {title}
+          </h3>
+          {showMoreIcon && moreUrl && (
             <h3
-              className="title"
+              className="more"
               style={{
                 backgroundColor: color,
                 filter: colorMode === 'dark' ? 'brightness(0.8)' : undefined,
               }}
-              suppressHydrationWarning
             >
-              <div className="absolute left-4 z-1 transform scale-120">
-                {icon}
-              </div>
-
-              {title}
+              <Link href={moreUrl}>
+                <IcBaselineArrowForwardIos />
+              </Link>
             </h3>
-            {showMoreIcon && moreUrl && (
-              <h3
-                className="more"
-                style={{
-                  backgroundColor: color,
-                  filter: colorMode === 'dark' ? 'brightness(0.8)' : undefined,
-                }}
-              >
-                <Link href={moreUrl}>
-                  <IcBaselineArrowForwardIos />
-                </Link>
-              </h3>
-            )}
-          </div>
-          <div className="news-body">
-            <div
-              className={clsx(
-                'grid sm:grid-cols-4 grid-cols-2 gap-4',
-                className,
-              )}
-              {...rest}
-            >
-              {props.children}
-            </div>
+          )}
+        </div>
+        <div className="news-body mt-4 mb-8">
+          <div
+            className={clsx('grid sm:grid-cols-4 grid-cols-2 gap-4', className)}
+            {...rest}
+          >
+            {props.children}
           </div>
         </div>
-      </>
+      </div>
     )
   }),
 )
