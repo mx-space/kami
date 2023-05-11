3c88af2c9e5edf5512379bf744221dea7230a582
Innei <tukon479@gmail.com>
2023-05-11 21:22:59 +0800

    chore: update float-ui
    Signed-off-by: Innei <tukon479@gmail.com>

--- packages/kami-design/components/FloatPopover/index.tsx
+++ packages/kami-design/components/FloatPopover/index.tsx
@@ -1,4 +1,4 @@
-import { clsx } from 'clsx'
+import clsx from 'clsx'
 import React, {
   memo,
   useCallback,
@@ -7,19 +7,17 @@ import React, {
   useRef,
   useState,
 } from 'react'
-import type { FC } from 'react'
+import type { FC, PropsWithChildren } from 'react'
 
-import type { UseFloatingProps } from '@floating-ui/react-dom'
+import type { UseFloatingOptions } from '@floating-ui/react-dom'
 import { flip, offset, shift, useFloating } from '@floating-ui/react-dom'
 import useClickAway from '@mx-space/kami-design/hooks/use-click-away'
-
-import { withNoSSR } from '~/components/biz/HoC/no-ssr'
 
 import { RootPortal } from '../Portal'
 import styles from './index.module.css'
 
 export const FloatPopover: FC<
-  {
+  PropsWithChildren<{
     triggerComponent: FC
     headless?: boolean
     wrapperClassNames?: string
@@ -32,202 +30,214 @@ export const FloatPopover: FC<
      * 不消失
      */
     debug?: boolean
-  } & UseFloatingProps
-> = withNoSSR(
-  memo((props) => {
-    const {
-      headless = false,
-      wrapperClassNames,
-      triggerComponent: TriggerComponent,
-      trigger = 'hover',
-      padding,
-      offset: offsetValue,
-      popoverWrapperClassNames,
-      debug,
-      ...floatingProps
-    } = props
 
-    const [mounted, setMounted] = useState(false)
+    animate?: boolean
 
-    const [currentStatus, setCurrentStatus] = useState(false)
-    const [open, setOpen] = useState(false)
-    const { x, y, reference, floating, strategy, update, isPositioned } =
-      useFloating({
-        middleware: floatingProps.middleware ?? [
-          flip({ padding: padding ?? 20 }),
-          offset(offsetValue ?? 10),
-          shift(),
-        ],
-        strategy: floatingProps.strategy,
-        placement: floatingProps.placement ?? 'bottom-start',
-        whileElementsMounted: floatingProps.whileElementsMounted,
+    as?: keyof HTMLElementTagNameMap
+  }> &
+    UseFloatingOptions
+> = memo((props) => {
+  const {
+    headless = false,
+    wrapperClassNames,
+    triggerComponent: TriggerComponent,
+    trigger = 'hover',
+    padding,
+    offset: offsetValue,
+    popoverWrapperClassNames,
+    debug,
+    animate = true,
+    as: As = 'div',
+    ...floatingProps
+  } = props
+
+  const [mounted, setMounted] = useState(false)
+
+  const [currentStatus, setCurrentStatus] = useState(false)
+  const [open, setOpen] = useState(false)
+  const { x, y, refs, strategy, update, isPositioned } = useFloating({
+    middleware: floatingProps.middleware ?? [
+      flip({ padding: padding ?? 20 }),
+      offset(offsetValue ?? 10),
+      shift(),
+    ],
+    strategy: floatingProps.strategy,
+    placement: floatingProps.placement ?? 'bottom-start',
+    whileElementsMounted: floatingProps.whileElementsMounted,
+  })
+  const updateOnce = useRef(false)
+  const doPopoverShow = useCallback(() => {
+    setCurrentStatus(true)
+    setMounted(true)
+
+    if (!updateOnce.current) {
+      requestAnimationFrame(() => {
+        update()
+        updateOnce.current = true
       })
-    const updateOnce = useRef(false)
-    const doPopoverShow = useCallback(() => {
-      setCurrentStatus(true)
-      setMounted(true)
+    }
+  }, [])
 
-      if (!updateOnce.current) {
-        requestAnimationFrame(() => {
-          update()
-          updateOnce.current = true
-        })
-      }
-    }, [])
+  const [containerAnchorRef, setContainerAnchorRef] =
+    useState<HTMLDivElement | null>()
+  const containerRef = useRef<HTMLDivElement>(null)
 
-    const [containerAnchorRef, setContainerAnchorRef] =
-      useState<HTMLDivElement | null>()
-    const containerRef = useRef<HTMLDivElement>(null)
+  const handleTransition = useCallback(
+    (status: 'in' | 'out') => {
+      const nextElementSibling =
+        containerAnchorRef?.nextElementSibling as HTMLDivElement
 
-    const handleTransition = useCallback(
-      (status: 'in' | 'out') => {
-        const nextElementSibling =
-          containerAnchorRef?.nextElementSibling as HTMLDivElement
-
-        if (!nextElementSibling) {
-          return
-        }
-
-        if (status === 'in') {
-          nextElementSibling.ontransitionend = null
-          nextElementSibling?.classList.add(styles.show)
-        } else {
-          nextElementSibling?.classList.remove(styles.show)
-          nextElementSibling!.ontransitionend = () => {
-            setOpen(false)
-            setMounted(false)
-          }
-        }
-      },
-      [containerAnchorRef?.nextElementSibling],
-    )
-
-    useEffect(() => {
-      if (!containerAnchorRef) {
+      if (!nextElementSibling) {
         return
       }
 
-      if (currentStatus) {
-        setOpen(true)
-        requestAnimationFrame(() => {
-          handleTransition('in')
-        })
+      if (status === 'in') {
+        nextElementSibling.ontransitionend = null
+        nextElementSibling?.classList.add(styles.show)
       } else {
-        requestAnimationFrame(() => {
-          handleTransition('out')
-        })
+        nextElementSibling?.classList.remove(styles.show)
+        nextElementSibling!.ontransitionend = () => {
+          setOpen(false)
+          setMounted(false)
+        }
       }
-    }, [currentStatus, containerAnchorRef, handleTransition])
+    },
+    [containerAnchorRef?.nextElementSibling],
+  )
 
-    useClickAway(containerRef, () => {
-      if (trigger == 'click' || trigger == 'both') {
-        doPopoverDisappear()
-        clickTriggerFlag.current = false
-      }
-    })
-
-    const doPopoverDisappear = useCallback(() => {
-      if (debug) {
-        return
-      }
-      setCurrentStatus(false)
-    }, [debug])
-
-    const clickTriggerFlag = useRef(false)
-    const handleMouseOut = useCallback(() => {
-      if (clickTriggerFlag.current === true) {
-        return
-      }
-      doPopoverDisappear()
-    }, [])
-    const handleClickTrigger = useCallback(() => {
-      clickTriggerFlag.current = true
-      doPopoverShow()
-    }, [])
-
-    const listener = useMemo(() => {
-      const baseListener = {
-        onFocus: doPopoverShow,
-        onBlur: doPopoverDisappear,
-      }
-      switch (trigger) {
-        case 'click':
-          return {
-            ...baseListener,
-            onClick: doPopoverShow,
-          }
-        case 'hover':
-          return {
-            ...baseListener,
-            onMouseOver: doPopoverShow,
-            onMouseOut: doPopoverDisappear,
-          }
-        case 'both':
-          return {
-            ...baseListener,
-            onClick: handleClickTrigger,
-            onMouseOver: doPopoverShow,
-            onMouseOut: handleMouseOut,
-          }
-      }
-    }, [
-      doPopoverDisappear,
-      doPopoverShow,
-      handleClickTrigger,
-      handleMouseOut,
-      trigger,
-    ])
-
-    const TriggerWrapper = (
-      <div
-        role={trigger === 'both' || trigger === 'click' ? 'button' : 'note'}
-        tabIndex={0}
-        className={clsx('inline-block', wrapperClassNames)}
-        ref={reference}
-        {...listener}
-      >
-        <TriggerComponent />
-      </div>
-    )
-
-    if (!props.children) {
-      return TriggerWrapper
+  useEffect(() => {
+    if (!containerAnchorRef) {
+      return
     }
 
-    return (
-      <>
-        {TriggerWrapper}
+    if (currentStatus) {
+      setOpen(true)
+      requestAnimationFrame(() => {
+        handleTransition('in')
+      })
+    } else {
+      requestAnimationFrame(() => {
+        handleTransition('out')
+      })
+    }
+  }, [currentStatus, containerAnchorRef, handleTransition])
 
-        {mounted && (
-          <RootPortal>
-            <div
-              className={clsx('float-popover', popoverWrapperClassNames)}
-              {...(trigger === 'hover' || trigger === 'both' ? listener : {})}
-              ref={containerRef}
-            >
-              <div ref={setContainerAnchorRef} />
-              {open && (
-                <div
-                  className={
-                    headless ? styles['headless'] : styles['popover-root']
-                  }
-                  ref={floating}
-                  style={{
-                    position: strategy,
-                    top: y ?? '',
-                    left: x ?? '',
-                    visibility:
-                      isPositioned && x !== null ? 'visible' : 'hidden',
-                  }}
-                  role="dialog"
-                >
-                  {props.children}
-                </div>
-              )}
-            </div>
-          </RootPortal>
-        )}
-      </>
-    )
-  }),
-)
+  useClickAway(containerRef, () => {
+    if (trigger == 'click' || trigger == 'both') {
+      doPopoverDisappear()
+      clickTriggerFlag.current = false
+    }
+  })
+
+  const doPopoverDisappear = useCallback(() => {
+    if (debug) {
+      return
+    }
+    if (!animate) {
+      setOpen(false)
+    }
+    setCurrentStatus(false)
+  }, [debug, animate])
+
+  const clickTriggerFlag = useRef(false)
+  const handleMouseOut = useCallback(() => {
+    if (clickTriggerFlag.current === true) {
+      return
+    }
+    doPopoverDisappear()
+  }, [])
+  const handleClickTrigger = useCallback(() => {
+    clickTriggerFlag.current = true
+    doPopoverShow()
+  }, [])
+
+  const listener = useMemo(() => {
+    const baseListener = {
+      onFocus: doPopoverShow,
+      onBlur: doPopoverDisappear,
+    }
+    switch (trigger) {
+      case 'click':
+        return {
+          ...baseListener,
+          onClick: doPopoverShow,
+        }
+      case 'hover':
+        return {
+          ...baseListener,
+          onMouseOver: doPopoverShow,
+          onMouseOut: doPopoverDisappear,
+        }
+      case 'both':
+        return {
+          ...baseListener,
+          onClick: handleClickTrigger,
+          onMouseOver: doPopoverShow,
+          onMouseOut: handleMouseOut,
+        }
+    }
+  }, [
+    doPopoverDisappear,
+    doPopoverShow,
+    handleClickTrigger,
+    handleMouseOut,
+    trigger,
+  ])
+
+  const TriggerWrapper = (
+    <As
+      // @ts-ignore
+      role={trigger === 'both' || trigger === 'click' ? 'button' : 'note'}
+      tabIndex={0}
+      className={clsx('inline-block', wrapperClassNames)}
+      ref={refs.setReference}
+      {...listener}
+    >
+      <TriggerComponent />
+    </As>
+  )
+
+  if (!props.children) {
+    return TriggerWrapper
+  }
+
+  return (
+    <>
+      {TriggerWrapper}
+
+      {mounted && (
+        <RootPortal>
+          <div
+            className={clsx(
+              'float-popover',
+              'relative z-[99]',
+              popoverWrapperClassNames,
+            )}
+            {...(trigger === 'hover' || trigger === 'both' ? listener : {})}
+            ref={containerRef}
+          >
+            <div ref={setContainerAnchorRef} />
+            {open && (
+              <div
+                className={clsx(
+                  headless ? styles['headless'] : styles['popover-root'],
+                  animate && styles['animate'],
+                )}
+                ref={refs.setFloating}
+                style={{
+                  position: strategy,
+                  top: y ?? '',
+                  left: x ?? '',
+                  visibility: isPositioned && x !== null ? 'visible' : 'hidden',
+                }}
+                role="dialog"
+              >
+                {props.children}
+              </div>
+            )}
+          </div>
+        </RootPortal>
+      )}
+    </>
+  )
+})
